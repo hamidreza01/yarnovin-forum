fd4ee9115f482e85170ed4cb11e1a4f7bf5e3f68
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-11-02 10:15:25 +0330

    Delete public directory

--- public/assets/js/profile.js
+++
@@ -1,6 +0,0 @@
-let card = document.getElementById("card-name");
-let userName = document.getElementById("name");
-let colors = ["#ec407a", "#ab47bc", "#ef5350", "#7e57c2", "#5c6bc0", "#ffa726"];
-const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
-card.style.background = colors[random(0, colors.length)];
-card.children[0].innerHTML = userName.innerHTML.substring(0, 2);
